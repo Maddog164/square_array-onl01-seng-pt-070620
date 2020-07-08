be065180af64fc14ef0b79c404c3b86bb1fb47fe
Denise Henderson <denise@henderson3.us>
2020-07-08 21:55:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   square_array=[]
   array.each do |number|
-    element = number**2
-    square_array.push(element)
-    puts array
-    puts square_array
+    #element = number**2
+    square_array.push(number**2)
+    #puts array
+    square_array
   end
 end
