dd0cbc418f95b6026277e5baff5a5e7bf7e25f82
Denise Henderson <denise@henderson3.us>
2020-07-08 21:58:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  square_array=[]
+  square_array = []
   array.each do |number|
     #element = number**2
     square_array.push(number**2)
@@ -7,3 +7,6 @@ def square_array(array)
     square_array
   end
 end
+
+array = [1,2,3]
+p square_array(array)
